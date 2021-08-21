bd7716512497c73430a9449b222dcdec0ebc747b
Harikrishnan Midhun <harikrishnanmidhun77@gmail.com>
2021-08-20 23:33:15 -0400

    MongoURI removed

--- src/server/config/keys.js
+++ src/server/config/keys.js
@@ -1,4 +1,3 @@
 module.exports = {
-  mongoURI:
-    "mongodb+srv://harikrishnan_midhun:igen_test_mongo@cluster0.iwigi.mongodb.net/igen-phase2-test?retryWrites=true&w=majority",
+  mongoURI: "",
 };
