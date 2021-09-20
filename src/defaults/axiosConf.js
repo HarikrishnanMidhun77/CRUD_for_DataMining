1c46c2acd6abf62ee5d5628844f9aa6f83dc5840
Harikrishnan Midhun <harikrishnanmidhun77@gmail.com>
2021-09-20 18:49:55 -0400

    Server seperated

--- src/defaults/axiosConf.js
+++ src/defaults/axiosConf.js
@@ -1,3 +1,3 @@
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:5000/";
+axios.defaults.baseURL = "https://datamining-crud-server.herokuapp.com/"; // "http://localhost:5000/";
 export default axios;
--- src/server/config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://DataMiningDb:LetsDoIt@cluster0.iwigi.mongodb.net/DataMiningProject?retryWrites=true&w=majority",
-};
